a7eb769df15321f99eba17c235feeeaa3961ef4d
eclipsenexus228 <eclipsenexus228@gmail.com>
2026-04-04 00:57:05 +0300

    Update grob.py

--- groby/grob.py
+++ groby/grob.py
@@ -4,18 +4,17 @@ from aiogram.filters import CommandStart
 from aiogram.types import WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-# Твой токен (не забудь сменить его через /revoke в @BotFather для безопасности!)
+# Твой токен
 TOKEN = "8706720051:AAG0-LW48AzHbzGjy0eQzwHpZIjmF_LhpN4"
 
-# Ссылка на твой хостинг, где лежит game.html
-# Пример: "https://username.github.io/mygame/game.html"
-WEB_APP_URL = "https://eclipsenexus228.github.io/gamegroby/groby/index.html" 
+# Твоя ссылка на игру
+WEB_APP_URL = "https://eclipsenexus228.github.io/gamegroby/groby/index.html"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
+async def cmd_start(message: types.Message):
     builder = InlineKeyboardBuilder()
     builder.row(types.InlineKeyboardButton(
         text="Играть в Могилу ⚰️", 
@@ -23,12 +22,16 @@ async def start(message: types.Message):
     )
     
     await message.answer(
-        f"Привет! Нажимай на кнопку, чтобы открыть игру.",
+        f"Привет! Нажми кнопку ниже, чтобы открыть игру.",
         reply_markup=builder.as_markup()
     )
 
 async def main():
+    print("Бот запущен...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот выключен")
