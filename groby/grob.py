9c250585d14a53427b7e6220da7e1be6c2f23556
eclipsenexus228 <eclipsenexus228@gmail.com>
2026-04-04 00:25:04 +0300

    Add files via upload

---
+++ groby/grob.py
@@ -0,0 +1,34 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import WebAppInfo
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+# Твой токен (не забудь сменить его через /revoke в @BotFather для безопасности!)
+TOKEN = "8706720051:AAG0-LW48AzHbzGjy0eQzwHpZIjmF_LhpN4"
+
+# Ссылка на твой хостинг, где лежит game.html
+# Пример: "https://username.github.io/mygame/game.html"
+WEB_APP_URL = "https://твой-сайт.com/game.html" 
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="Играть в Могилу ⚰️", 
+        web_app=WebAppInfo(url=WEB_APP_URL))
+    )
+    
+    await message.answer(
+        f"Привет! Нажимай на кнопку, чтобы открыть игру.",
+        reply_markup=builder.as_markup()
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
